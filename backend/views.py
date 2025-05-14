d30efeab4d56447085d5f8ee2c5a9ac2a9a514ee
Hai <hoanghai050605@gmail.com>
2025-05-14 14:10:05 +0700

    basic auth backend

--- backend/views.py
+++ backend/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import *
+from .serializers import *
+from rest_framework import status
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
+class LoginView(APIView):
+    def post(self, request):
+        username = request.data["username"]
+        password = request.data["password"]
+        user = authenticate(request, username=username, password=password)
+
+        if user is None:
+            return Response({"message": "Invalid credentials"})
+        else:
+            login(request, user)
+            return Response({"message": "Login successful"})
+
+class LogoutView(APIView):
+    def post(self,request):
+        logout(request)
+        return Response({"message": "Logged out"})
+
+    
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)   
+        if serializer.is_valid():
+            serializer.save()
+            user = User.objects.get(username = request.data['username'])
+            user.set_password(request.data['password'])
+            user.save()
+            return Response(
+                {"message": "Registration successful"})
+        else:
+            error_msg = ""
+            for key in serializer.errors:
+                error_msg += serializer.errors[key][0]
+            return Response({"message": error_msg})
+        
+class HomeView(APIView):
+    def get(self, request):
+        if request.user.is_authenticated:
+            user = UserSerializer(request.user)
+            return Response(user.data)
+        else: 
+            return Response({"message": "no one logged in rn"})
+
