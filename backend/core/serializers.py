3c24ef2232550918d242def8c7790f07fac4c036
Hai <hoanghai050605@gmail.com>
2025-05-26 22:18:57 +0700

    added password requirements

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -1,11 +1,20 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError as DjangoValidationError
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'password', 'email']
         extra_kwargs = {"password": {"write_only": True}}
+
+    def validate_password(self, value):
+        try:
+            validate_password(value)
+        except DjangoValidationError as e:
+            raise serializers.ValidationError(e.messages)
+        return value
 
     def create(self, validated_data):
         print(validated_data)
