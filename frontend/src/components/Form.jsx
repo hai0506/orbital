503a69ba4f9f517fc7169b9b2a605ae4cd817e50
ruissouw <rohit16@live.com>
2025-05-29 12:19:17 +0800

    add user type in registration form

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -6,6 +6,7 @@ import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
 const Form = ({route, method}) => {
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
+    const [userType, setUserType] = useState("Organization")
     const [loading, setLoading] = useState(false)
     const navigate = useNavigate()
 
@@ -16,7 +17,13 @@ const Form = ({route, method}) => {
         e.preventDefault();
 
         try {
-            const res = await api.post(route, {username, password})
+            const info = {username, password}
+
+            if (method !== 'login') {
+                info.user_type = userType
+            }
+
+            const res = await api.post(route, info)
             if (method === "login") {
                 localStorage.setItem(ACCESS_TOKEN, res.data.access)
                 localStorage.setItem(REFRESH_TOKEN, res.data.refresh)
@@ -32,26 +39,38 @@ const Form = ({route, method}) => {
     }
 
     return <form onSubmit={handleSubmit} className="form-container">
-        <h1>{name}</h1> 
-        <input
-            className="form-input"
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            placeholder="Username"
-        />
-        
-        <input
-            className="form-input"
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="Password"
-        />
-        <button className="form-button" type="submit">
-            {name}
-        </button>
-    </form>
+            <h1>{name}</h1> 
+            <input
+                className="form-input"
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                placeholder="Username"
+            />
+            
+            <input
+                className="form-input"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
+            />
+            {method === "register" && (
+                <select
+                    className="form-input"
+                    value={userType}
+                    onChange={(e) => setUserType(e.target.value)}
+                    required
+                >
+                    <option value="Organization">I am an organization!</option>
+                    <option value="Vendor">I am a vendor!</option>
+                </select>
+            )}
+            <button className="form-button" type="submit">
+                {name}
+            </button>
+        </form>
+    
 }
 
 export default Form
