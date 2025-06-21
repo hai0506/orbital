58297e33943466b01b50c3b7b40f4368f74aa6c0
ruissouw <rohit16@live.com>
2025-06-21 10:33:22 +0800

    add key to each listing

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,7 +1,7 @@
 import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import Listing from './Listing'
-import listings from './Data'
+import listings from '../data/Listings'
 
 const user = {
   name: 'Tom Cook',
@@ -173,9 +173,13 @@ export default function Layout({method}) {
           </div>
         </header>
         <main>
-            <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 grid grid-cols-3 gap-4">
-                {listings.map(listing => <Listing fields={listing} />)}
-            </div>
+            {method === "Listings" && (
+              <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 grid grid-cols-3 gap-4">
+                {listings.map((listing, index) => (
+                    <Listing key={listing.id || index} fields={listing} />
+                ))}
+              </div>
+            )}
         </main>
       </div>
     </>
