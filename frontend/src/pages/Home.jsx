62442d8dbb192df6e082af5588378d85a498299e
ruissouw <rohit16@live.com>
2025-06-22 15:50:31 +0800

    retrieve role from local storage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,24 +2,22 @@ import React, { useEffect, useState } from 'react';
 import api from '../api';
 import Layout from "../components/Layout";
 import Listing from '../components/Listing';
+// JSON mock data
 import listings from '../data/Listings';
 
 const Home = () => {
-  const [role, setRole] = useState(null);
   //const [listings, setListings] = useState([]);
-  const [loading, setLoading] = useState(true);
+  //const [loading, setLoading] = useState(true);
+  const role = localStorage.getItem("ROLE");
 
+  /*
   useEffect(() => {
-    async function fetchProfileAndListings() {
+    async function fetchListings() {
       try {
-        const userProfile = await api.get('core/user/profile/');
-        setRole(userProfile.data.role);
-        /*
-        if (profileRes.data.role === 'vendor') {
+        if (role === 'vendor') {
           const listingsRes = await api.get('core/posts/');
           setListings(listingsRes.data);
         }
-        */
       } catch (error) {
         console.error('Failed to load profile or listings:', error);
       } finally {
@@ -27,12 +25,13 @@ const Home = () => {
       }
     }
 
-    fetchProfileAndListings();
+    fetchListings();
   }, []);
+  */
 
   return (
     <>
-      <Layout method="Listings">
+      <Layout heading="Listings">
         {role === "vendor" && (
           <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 grid grid-cols-3 gap-4">
             {listings.map((listing, index) => (
