aa2129612f57bce017204206955f2ee3876aa0dc
Hai <hoanghai050605@gmail.com>
2025-06-24 21:59:20 +0700

    fixed categories not showing in offer form

--- frontend/src/components/Listing.jsx
+++ frontend/src/components/Listing.jsx
@@ -54,7 +54,7 @@ const Listing = ({fields}) => {
                             </div>
 
                             <div className="w-[70%] border-l border-gray-300 p-4">
-                                <MakeOffer dates={dates} categories={fields.category_list}  />
+                                <MakeOffer dates={dates} categories={fields.categories}  />
                             </div>
                         </div>
 
