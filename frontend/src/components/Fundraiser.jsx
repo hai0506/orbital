68862144eee885fe4436462c916b2266ba59d546
ruissouw <rohit16@live.com>
2025-06-28 00:49:03 +0800

    create basic fundraiser component

---
+++ frontend/src/components/Fundraiser.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import { X } from "lucide-react";
+import { Button } from "@headlessui/react";
+import ListingDetails from "./ListingDetails";
+
+const Fundraiser = ({fundraiser}) => {
+    const [hovered, setHovered] = useState(false);
+    const [open, setOpen] = useState(false);
+
+    return (
+        <>
+            <div onMouseEnter={() => setHovered(true)} 
+                onMouseLeave={() => setHovered(false)} 
+                className="border border-gray-300 rounded-lg p-4 shadow-sm bg-white max-w-md"
+            >
+                <ListingDetails fields={fields} />
+
+                <button
+                    className="absolute top-2 right-2 text-gray-500 hover:text-black"
+                >
+                    <X/>
+                </button>
+
+                {hovered && (
+                    <Button onClick={() => {setOpen(true);setHovered(false);}} style={{ marginTop: "10px" }} className="inline-flex items-center gap-2 rounded-md bg-gray-700 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:not-data-focus:outline-none data-focus:outline data-focus:outline-white data-hover:bg-gray-600 data-open:bg-gray-700">
+                        Check it out!
+                    </Button>
+                )}
+            </div>
+
+            {open && (
+                <div 
+                    className="fixed inset-0 z-50 flex items-center justify-center bg-black/30 backdrop-blur-sm"
+                    onClick={() => setOpen(false)}
+                >
+                    <div 
+                        className="max-w-2xl w-full rounded-lg bg-white p-6 shadow-lg relative"
+                        onClick={e => e.stopPropagation()}
+                    >
+                        <div className="flex h-full w-full">
+                            <div className="w-[50%] p-4">
+                                <ListingDetails fields={fields} />
+                            </div>
+
+                            <div className="w-[70%] border-l border-gray-300 p-4">
+                                fundraisers
+                            </div>
+                        </div>
+
+                        <button
+                            onClick={() => setOpen(false)}
+                            className="absolute top-2 right-2 text-gray-500 hover:text-black"
+                        >
+                            <X/>
+                        </button>
+                    </div>
+                </div>
+            )}
+        </>
+    );
+};
+
+export default Fundraiser;
