700304bd9c502840346e7438476f4f8b3c9900eb
ruissouw <rohit16@live.com>
2025-06-27 17:40:45 +0800

    form to accept/reject offer

--- frontend/src/components/Offer.jsx
+++ frontend/src/components/Offer.jsx
@@ -1,12 +1,36 @@
 import { useState } from "react";
 import { X, BriefcaseBusiness, CircleCheckBig, CircleX } from "lucide-react";
-import { Description, Field, Fieldset, Input, Label, Button, Select, Textarea, Checkbox } from '@headlessui/react'
-import MakeOffer from "./MakeOffer";
+import api from "../api";
+import { Field, Label, Button } from '@headlessui/react'
 import ListingDetails from "./ListingDetails";
 
-const Offer = ({fields}) => {
+const Offer = ({offer}) => {
     const [hovered, setHovered] = useState(false);
     const [open, setOpen] = useState(false);
+    const [errors, setErrors] = useState({});
+    const [loading, setLoading] = useState(false);
+
+    const handleSubmit = async (status) => {
+        setLoading(true);
+        console.log("Accepting/rejecting offer")
+
+        try {
+            const info = {
+                offer: offer.offer_id,
+                status: status
+            }
+
+            console.log("Sending info:", info);
+            const route = ""; // change this if needed
+            const res = await api.post(route, info);
+            setOpen(false);
+        } catch (error) {
+            console.log(error)
+            setErrors(error.response.data)
+        } finally {
+            setLoading(false)
+        }
+    }
 
     return (
         <>
@@ -14,11 +38,11 @@ const Offer = ({fields}) => {
                 onMouseLeave={() => setHovered(false)} 
                 className="border border-gray-300 rounded-lg p-4 shadow-sm bg-white max-w-md"
             >
-                <h3 className="text-lg font-semibold text-gray-900 mb-3">{fields.vendor.username}</h3>
+                <h3 className="text-lg font-semibold text-gray-900 mb-3">{offer.vendor.username}</h3>
                 <dl style={{ marginTop: "10px" }} className="space-y-2">
                     <div className="flex text-sm text-gray-700">
                         <BriefcaseBusiness className="mr-2" />
-                        <dt className="font-medium">{fields.listing.title}</dt>
+                        <dt className="font-medium">{offer.listing.title}</dt>
                     </div>
                 </dl>
 
@@ -46,14 +70,14 @@ const Offer = ({fields}) => {
                     >
                         <div className="flex h-full w-full">
                             <div className="w-[50%] p-4">
-                                <ListingDetails fields={fields.listing} />
+                                <ListingDetails fields={offer.listing} />
                             </div>
 
-                            <form className="w-[70%] border-l border-gray-300 p-4">
-                                <Fieldset className="w-full max-w-lg px-4">
+                            <div className="w-[70%] border-l border-gray-300 p-4">
+                                <offeret className="w-full max-w-lg px-4">
                                     <Field>
                                         <Label className="text-base/7 font-medium text-black">Dates</Label>
-                                        {fields.allDays === "Yes" ? (
+                                        {offer.allDays === "Yes" ? (
                                             <div className="flex items-center gap-2 text-sm text-green-600">
                                                 <CircleCheckBig className="size-4" />
                                                 <span>Able to make it on all days</span>
@@ -65,7 +89,7 @@ const Offer = ({fields}) => {
                                                     <span>Unable to make it on these days:</span>
                                                 </div>
                                                     <ul className="pl-6 list-none space-y-1">
-                                                        {fields.selectedDays.map((day, index) => (
+                                                        {offer.selectedDays.map((day, index) => (
                                                             <li key={index} className="flex items-center gap-2">
                                                             <span>{new Intl.DateTimeFormat('en-GB').format(day)}</span>
                                                             </li>
@@ -77,9 +101,9 @@ const Offer = ({fields}) => {
                                     <Field>
                                         <Label className="text-base/7 font-medium text-black">Products</Label>
                                         <div className="flex flex-wrap gap-3">
-                                            {(fields.selectedCategories ?? []).map((category) => (
+                                            {(offer.selectedCategories ?? []).map((category) => (
                                                 <span
-                                                    className={`inline-block rounded-full px-3 py-1 text-xs font-semibold ${fields.listing.categories.includes(category) ? "bg-green-100 text-green-800" : "bg-orange-100 text-orange-800"}`}
+                                                    className={`inline-block rounded-full px-3 py-1 text-xs font-semibold ${offer.listing.categories?.includes(category) ? "bg-green-100 text-green-800" : "bg-orange-100 text-orange-800"}`}
                                                 >
                                                     {category}
                                                 </span>
@@ -88,41 +112,41 @@ const Offer = ({fields}) => {
                                     </Field>
                                     <Field>
                                         <Label className="text-base/7 font-medium text-black">Commission</Label>
-                                        {fields.commission >= fields.listing.commission 
+                                        {offer.commission >= offer.listing.commission 
                                             ?   ( 
                                                     <div className="flex items-center gap-2 text-sm text-green-600">
                                                         <CircleCheckBig className="size-4" />
-                                                        <span>{fields.commission}% of revenue</span>
+                                                        <span>{offer.commission}% of revenue</span>
                                                     </div>
                                                 )
                                             :   ( 
                                                     <div className="flex items-center gap-2 text-sm text-red-600">
                                                         <CircleX className="size-4" />
-                                                        <span>{fields.commission}% of revenue ({fields.listing.commission - fields.commission}% less)</span>
+                                                        <span>{offer.commission}% of revenue ({offer.listing.commission - offer.commission}% less)</span>
                                                     </div>
                                                 )
                                         }
                                     </Field>
                                     <Field>
-                                        {fields.remarks && (
+                                        {offer.remarks && (
                                             <>
                                                 <Label className="text-base/7 font-medium text-black">Remarks</Label>
                                                 <div className="flex items-center gap-2 text-sm">
-                                                    {fields.remarks}
+                                                    {offer.remarks}
                                                 </div>
                                             </>
                                         )}
                                     </Field>
 
-                                    <Button style={{ marginTop: "10px", marginRight: "10px" }} className="inline-flex items-center gap-2 rounded-md bg-green-700 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:not-data-focus:outline-none data-focus:outline data-focus:outline-white data-hover:bg-green-600 data-open:bg-green-700">
+                                    <Button onClick={() => handleSubmit("accepted")} style={{ marginTop: "10px", marginRight: "10px" }} className="inline-flex items-center gap-2 rounded-md bg-green-700 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:not-data-focus:outline-none data-focus:outline data-focus:outline-white data-hover:bg-green-600 data-open:bg-green-700">
                                         Accept Offer
                                     </Button>
 
-                                    <Button style={{ marginTop: "10px" }} className="inline-flex items-center gap-2 rounded-md bg-red-700 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:not-data-focus:outline-none data-focus:outline data-focus:outline-white data-hover:bg-red-600 data-open:bg-red-700">
+                                    <Button onClick={() => handleSubmit("rejected")} style={{ marginTop: "10px" }} className="inline-flex items-center gap-2 rounded-md bg-red-700 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:not-data-focus:outline-none data-focus:outline data-focus:outline-white data-hover:bg-red-600 data-open:bg-red-700">
                                         Reject Offer
                                     </Button>
-                                </Fieldset>
-                            </form>
+                                </offeret>
+                            </div>
                         </div>
 
                         <button
