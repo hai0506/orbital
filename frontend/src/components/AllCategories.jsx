35825fcf58f11f4518838700f951a56ff194e030
ruissouw <rohit16@live.com>
2025-06-29 13:19:35 +0800

    create list of all categories

---
+++ frontend/src/components/AllCategories.jsx
@@ -0,0 +1,16 @@
+ const allCategories = [
+    "Food & Beverages",
+    "Accessories",
+    "Stationery",
+    "Clothing",
+    "Toys",
+    "Books",
+    "Home Decor",
+    "Art & Crafts",
+    "Tech Gadgets",
+    "Skincare & Beauty",
+    "Plants",
+    "Pet Supplies",
+];
+
+export default allCategories;
