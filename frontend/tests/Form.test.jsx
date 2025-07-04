3d9097c252957a9627d18328c556d392dd2c7802
ruissouw <rohit16@live.com>
2025-07-04 10:26:22 +0800

    test all form components

--- frontend/tests/Form.test.jsx
+++ frontend/tests/Form.test.jsx
@@ -5,7 +5,7 @@ import { MemoryRouter } from "react-router-dom";
 import Form from "../src/components/Form";
 import "@testing-library/jest-dom/vitest"
 
-describe("Login", () => {
+describe("Login and Register Form", () => {
   it("renders a login page", () => {
     render(
       <MemoryRouter>
@@ -13,7 +13,11 @@ describe("Login", () => {
       </MemoryRouter>
     );
     
-    expect(screen.getByText("Sign in to your account")).toBeInTheDocument();
+    expect(screen.getByText(/Sign in to your account/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/Username/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/Password/i)).toBeInTheDocument()
+    expect(screen.getByRole("button", { name: /Sign in/i })).toBeInTheDocument()
+    expect(screen.getByText(/Don't have an account/i)).toBeInTheDocument()
   });
 
   it("renders a register page", () => {
@@ -23,6 +27,12 @@ describe("Login", () => {
       </MemoryRouter>
     );
     
-    expect(screen.getByText("Register your account")).toBeInTheDocument();
+    expect(screen.getByText(/Register your account/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/Email/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/Username/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/Password/i)).toBeInTheDocument()
+    expect(screen.getByText(/Are you an organization or vendor/i)).toBeInTheDocument()
+    expect(screen.getByRole("button", { name: /Register/i })).toBeInTheDocument()
+    expect(screen.getByText(/Already have an account/i)).toBeInTheDocument()
   });
 });
