6471301bd89bb22f86498a46a2e515caea62a9d5
ruissouw <rohit16@live.com>
2025-07-25 01:28:35 +0800

    add vendor review tab

--- frontend/src/pages/VendorFundraiser.jsx
+++ frontend/src/pages/VendorFundraiser.jsx
@@ -23,6 +23,7 @@ import {
 import { MoveLeft, MoveRight, X } from "lucide-react";
 import CountdownClock from '@/components/CountdownClock';
 import Dashboard from '@/components/Dashboard';
+import Review from '@/components/Review';
 
 // comment this out to test api
 // import transactions from '@/data/Transactions';
@@ -198,6 +199,9 @@ const VendorFundraiser = () => {
                                 <TabsTrigger value="inventory">Inventory</TabsTrigger>
                                 <TabsTrigger value="transactions">Transactions</TabsTrigger>
                                 <TabsTrigger value="statistics">Statistics</TabsTrigger>
+                                {fundraiser.status == "concluded" && (
+                                    <TabsTrigger value="review">Review</TabsTrigger>
+                                )}
                             </TabsList>
                             <TabsContent value="inventory">
                                 <>
@@ -520,6 +524,11 @@ const VendorFundraiser = () => {
                             <TabsContent value="statistics">
                                 <Dashboard fundraiser={fundraiser} />
                             </TabsContent>
+                            {fundraiser.status == "concluded" && (
+                                <TabsContent value="review">
+                                    <Review fundraiser={fundraiser} />
+                                </TabsContent>
+                            )}
                         </Tabs>
                     </div>
                 </div>
