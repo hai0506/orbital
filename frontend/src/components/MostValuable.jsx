ee8be5116e5b2de05307fa9fb51c4fe1db6a7caf
ruissouw <rohit16@live.com>
2025-07-24 22:21:41 +0800

    create component for each pie chart

---
+++ frontend/src/components/MostValuable.jsx
@@ -0,0 +1,59 @@
+import { useState } from 'react'
+import { PieChart, Pie, Cell, Tooltip } from "recharts";
+
+const MostSold = ({ data, total }) => {
+    const COLORS = ["#4CAF50", "#2196F3", "#FF9800", "#ccc"];
+    const [activeIndex, setActiveIndex] = useState(null);
+
+    const onPieEnter = (_, index) => setActiveIndex(index);
+    const onPieLeave = () => setActiveIndex(null);
+
+    return (
+        <div style={{ position: "relative", width: 200, height: 200 }}>
+            <PieChart width={200} height={200}>
+                <Pie
+                    data={data}
+                    cx="50%"
+                    cy="50%"
+                    innerRadius={60}
+                    outerRadius={80}
+                    fill="#8884d8"
+                    dataKey="value"
+                    onMouseEnter={onPieEnter}
+                    onMouseLeave={onPieLeave}
+                >
+                    {data.map((entry, index) => (
+                        <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                    ))}
+                </Pie>
+
+                <Tooltip
+                    position={{ x: 220, y: 100 }} 
+                    contentStyle={{ fontSize: '12px' }}
+                />
+            </PieChart>
+
+            <div
+                style={{
+                    position: "absolute",
+                    top: "50%",
+                    left: "50%",
+                    transform: "translate(-50%, -50%)",
+                    fontSize: 21,
+                    fontWeight: "bold",
+                    pointerEvents: "none",
+                    width: 100,          
+                    textAlign: "center", 
+                    lineHeight: 1.2,     
+                    userSelect: "none",  
+                }}
+            >
+                {activeIndex !== null
+                    ? `${data[activeIndex].name} ($${data[activeIndex].value})`
+                    : `$${total} Total`}
+            </div>
+        </div>
+    )
+}
+
+export default MostSold;
