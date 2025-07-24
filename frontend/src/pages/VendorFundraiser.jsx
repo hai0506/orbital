7476c9a0185be17db7e181be6d1922f9d55d42b6
ruissouw <rohit16@live.com>
2025-07-24 22:22:40 +0800

    refresh stats page when sale made

--- frontend/src/pages/VendorFundraiser.jsx
+++ frontend/src/pages/VendorFundraiser.jsx
@@ -45,20 +45,20 @@ const VendorFundraiser = () => {
     const [searchBuyer, setSearchBuyer] = useState("");
     const [errors, setErrors] = useState({});
     
-    useEffect(() => {
-        async function fetchFundraiser() {
-            try {
-                const fundraiserRes = await api.get(`core/fundraiser/${id}`);
-                setFundraiser(fundraiserRes.data);
-                setFullInventory(fundraiserRes.data.inventory);
-                setInventory(fundraiserRes.data.inventory);
-                setOngoing(fundraiserRes.data.status == "ongoing");
-                console.log(fundraiserRes.data);
-            } catch (error) {
-                console.error('Failed to load fundraiser:', error);
-            }
+    async function fetchFundraiser() {
+        try {
+            const fundraiserRes = await api.get(`core/fundraiser/${id}`);
+            setFundraiser(fundraiserRes.data);
+            setFullInventory(fundraiserRes.data.inventory);
+            setInventory(fundraiserRes.data.inventory);
+            setOngoing(fundraiserRes.data.status == "ongoing");
+            console.log(fundraiserRes.data);
+        } catch (error) {
+            console.error('Failed to load fundraiser:', error);
         }
+    }
 
+    useEffect(() => {
         fetchFundraiser();
     }, []);
 
@@ -153,6 +153,7 @@ const VendorFundraiser = () => {
             setCart([]);
             setBuyerDetails({});
             await fetchTransactions();
+            await fetchFundraiser();
         } catch (error) {
             console.error('Failed to update inventory:', error);
             setErrors(error.response.data)
