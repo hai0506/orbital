d4cdc7844130149de1b7159b9a8068b03a496e48
ruissouw <rohit16@live.com>
2025-07-24 14:05:35 +0800

    filter profiles based on search

--- frontend/src/pages/Search.jsx
+++ frontend/src/pages/Search.jsx
@@ -1,33 +1,59 @@
 import Layout from '@/components/Layout';
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
+import { Input } from "@/components/ui/input";
+import UserProfile from '@/components/UserProfile';
+import api from '@/api';
 
 const Search = () => {
-    const searchUser = useState("");
+    const [searchUser, setSearchUser] = useState("");
     const [loading, setLoading] = useState(false);
+    const [allProfiles, setAllProfiles] = useState([]);
+    const [profiles, setProfiles] = useState([]);
 
+    // /*
     useEffect(() => {
         async function fetchProfiles() {
             setLoading(true);
             try {
-                const profileRes = await api.get('core/profile/');
-                setProfile(profileRes.data);
-                setOriginal(profileRes.data);
-                setPfpPreview(profileRes.data.pfp);
+                const profilesRes = await api.get('core/profiles/');
+                setAllProfiles(profilesRes.data);
+                setProfiles(profilesRes.data);
+                console.log(allProfiles);
             } catch (error) {
-                console.error('Failed to load profile:', error);
+                console.error('Failed to load profiles:', error);
             } finally {
                 setLoading(false);
             }
         }
+
+        fetchProfiles();
     }, [])
+    // */
+
+    useEffect(() => {
+        const filtered = allProfiles.filter(profile =>
+            profile.username.toLowerCase().includes(searchUser.toLowerCase())
+        );
+        setProfiles(filtered);
+    }, [searchUser, allProfiles]);
 
     return (
         <Layout heading="Search User">
             <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 mt-3">
-
+                <Input
+                    type="search"
+                    placeholder="Search for Vendor or Organisation"
+                    onChange={e => setSearchUser(e.target.value)}
+                    className="w-full md:w-1/3 mb-4"
+                />
+                <div className="grid grid-cols-3 gap-4">
+                    {profiles?.map(profile => (
+                        <UserProfile profile={profile} />
+                    ))}
+                </div>  
             </div>
         </Layout>
     )
 }
 
-export default Search
+export default Search;
