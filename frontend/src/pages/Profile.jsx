61761861e5d841447e9c376cf575fc5b966fe0d4
ruissouw <rohit16@live.com>
2025-07-24 14:25:37 +0800

    add chat button

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -216,10 +216,12 @@ const Profile = () => {
                             </button>
                         </div>
                     )}
+                    {id && (
+                        <Button onClick={() => navigate("/chat", {state:{receiverId: id}})} style={{ marginTop: "10px" }} className="inline-flex items-center gap-2 rounded-md bg-gray-700 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:not-data-focus:outline-none data-focus:outline data-focus:outline-white data-hover:bg-gray-600 data-open:bg-gray-700">
+                            {`Contact ${profile.user_type}`}
+                        </Button>
+                    )}
                 </form>
-                <Button>
-                    hello
-                </Button>
             </div>
         </Layout>
     );
