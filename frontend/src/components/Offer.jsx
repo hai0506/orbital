3fc5c0111a8d206db983c1a315ac2fdb3bb9aa00
ruissouw <rohit16@live.com>
2025-07-25 14:28:22 +0800

    remove cancelled offers

--- frontend/src/components/Offer.jsx
+++ frontend/src/components/Offer.jsx
@@ -181,11 +181,11 @@ const Offer = ({ offer, onChangeStatus }) => {
                                     )}
 
                                     {status == "cancelled" && (
-                                        <Button onClick={() => handleSubmit("rejected")} style={{ marginTop: "10px" }} className="inline-flex items-center gap-2 rounded-md bg-red-700 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:not-data-focus:outline-none data-focus:outline data-focus:outline-white data-hover:bg-red-600 data-open:bg-red-700">
+                                        <Button onClick={handleRemoval} style={{ marginTop: "10px" }} className="inline-flex items-center gap-2 rounded-md bg-red-700 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:not-data-focus:outline-none data-focus:outline data-focus:outline-white data-hover:bg-red-600 data-open:bg-red-700">
                                             Remove Offer
                                         </Button>
                                     )}
-                                    
+
                                     <Button type="button" style={{ marginTop: "10px" }} className="ml-8 inline-flex items-center gap-2 rounded-md bg-gray-700 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:not-data-focus:outline-none data-focus:outline data-focus:outline-white data-hover:bg-gray-600 data-open:bg-gray-700"
                                         onClick={() => navigate("/chat", {state:{receiverId: offer.vendor.userid}})} >
                                         Contact Vendor
