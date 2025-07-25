c485a598dc85e54ed4f89bab46dfb08dd260c11c
ruissouw <rohit16@live.com>
2025-07-25 14:32:18 +0800

    remove X in absolute right corner

--- frontend/src/components/VendorOffer.jsx
+++ frontend/src/components/VendorOffer.jsx
@@ -47,13 +47,6 @@ const VendorOffer = ({ offer, deleteOffer }) => {
             >
                 <ListingDetails fields={offer.listing} status={status} />
 
-
-                <button
-                    className="absolute top-2 right-2 text-gray-500 hover:text-black"
-                >
-                    <X/>
-                </button>
-
                 {hovered && (
                     <Button onClick={() => {setOpen(true);setHovered(false);}} style={{ marginTop: "10px" }} className="inline-flex items-center gap-2 rounded-md bg-gray-700 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:not-data-focus:outline-none data-focus:outline data-focus:outline-white data-hover:bg-gray-600 data-open:bg-gray-700">
                         Check it out!
