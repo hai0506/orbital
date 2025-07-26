f0017e8f00836eb90050c10d889cf92eb8f6495b
ruissouw <rohit16@live.com>
2025-07-26 11:36:19 +0800

    refresh token

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,67 +1,19 @@
-import {Navigate} from "react-router-dom";
-import {jwtDecode} from "jwt-decode";
-import api from "../api";
-import { REFRESH_TOKEN, ACCESS_TOKEN } from "../constants";
-import { useState, useEffect } from "react";
- 
+import { Navigate } from "react-router-dom";
+import { ACCESS_TOKEN } from "../constants";
+
 function ProtectedRoute({ children, authRoles }) {
-    const [isAuthorized, setIsAuthorized] = useState(null);
+    const token = localStorage.getItem(ACCESS_TOKEN);
     const role = localStorage.getItem("ROLE");
 
-    useEffect(() => {
-        auth().catch(() => setIsAuthorized(false))
-    }, [])
-
-    const refreshToken = async () => {
-        const refreshToken = localStorage.getItem(REFRESH_TOKEN);
-        try {
-            const res = await api.post("/api/token/refresh/", {
-                refresh: refreshToken,
-            });
-            if (res.status === 200) {
-                localStorage.setItem(ACCESS_TOKEN, res.data.access)
-                setIsAuthorized(true)
-            } else {
-                setIsAuthorized(false)
-            }
-        } catch (error) {
-            console.log(error)
-            setIsAuthorized(false)
-        }
-    }
-
-    const auth = async () => {
-        const token = localStorage.getItem(ACCESS_TOKEN)
-        if (!token) {
-            setIsAuthorized(false)
-            return
-        }
-        const decoded = jwtDecode(token)
-        const tokenExpiration = decoded.tokenExpiration
-        const now = Date.now() / 1000
-
-        if (tokenExpiration < now) {
-            await refreshToken()
-        } else {
-            setIsAuthorized(true)
-        }
-    }
-
-    if (isAuthorized == null) {
-        return <div>
-            Loading...
-        </div>;
-    }
-
-    if (!isAuthorized) {
-        return <Navigate to="/login" />
+    if (!token) {
+        return <Navigate to="/login" />;
     }
 
     if (!authRoles || authRoles.includes(role)) {
         return children;
-    } else {
-        return <Navigate to="/login" />
     }
+
+    return <Navigate to="/login" />;
 }
 
 export default ProtectedRoute;
