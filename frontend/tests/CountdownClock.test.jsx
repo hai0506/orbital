fd960a48f63e003f59c80b1f123c0815d12f2ce8
ruissouw <rohit16@live.com>
2025-07-27 11:22:44 +0800

    write test for countdown clock

--- frontend/tests/CountdownClock.test.jsx
+++ frontend/tests/CountdownClock.test.jsx
@@ -10,69 +10,46 @@ afterEach(() => {
 
 
 describe("countdown clock", () => {
-    const today = new Date();
-    today.setDate(today.getDate() + 1); 
-
-    const yyyy = today.getFullYear();
-    const mm = String(today.getMonth() + 1).padStart(2, '0');
-    const dd = String(today.getDate()).padStart(2, '0');
-
-    const start_time = `${yyyy}-${mm}-${dd}`;
-
-    it("renders countdown to fundraiser when it is yet to start", () => {
+    it("renders countdown to fundraiser when it is yet to start", async () => {
         render(
             <MemoryRouter>
                 <CountdownClock 
-                    startTime={`${start_time}T18:27:00`}
-                    endTime={`${fundraiser?.offer.listing.end_date}T18:27:00`} 
+                    startTime={`${"9999-07-26"}T18:27:00`}
+                    endTime={`${"9999-07-27"}T18:27:00`} 
                 />
             </MemoryRouter>
         )
 
-        // listing name visible
-        expect(screen.getByText("ListingDetailsMock")).toBeInTheDocument()
-
-        // button hidden until hover
-        expect(screen.queryByText("Check it out!")).not.toBeInTheDocument()
-
-        // hover over component
-        fireEvent.mouseEnter(screen.getByText("ListingDetailsMock").parentElement)
-
-        expect(screen.getByText("Check it out!")).toBeInTheDocument()
-
-        // open component
-        fireEvent.click(screen.getByText("Check it out!"))
-        expect(screen.getAllByText("ListingDetailsMock")).toHaveLength(2)
-
-        // see congrats 
-        expect(screen.getByText("Congratulations on the offer!")).toBeInTheDocument();
+        expect(await screen.findByText("Fundraiser starts in:")).toBeInTheDocument();
+        expect(screen.queryByText("Fundraiser ends in:")).not.toBeInTheDocument();
     })
 
-    it("shows offer card, reveals button on hover, opens modal, see rejection message", () => {
-        const deleteOffer = vi.fn()
-
+    it("renders countdown to fundraiser end when ongoing", async () => {
         render(
-        <MemoryRouter>
-            <VendorOffer offer={rejectedOffer} deleteOffer={deleteOffer} />
-        </MemoryRouter>
+            <MemoryRouter>
+                <CountdownClock 
+                    startTime={`${"2004-07-26"}T18:27:00`}
+                    endTime={`${"9999-07-27"}T18:27:00`} 
+                />
+            </MemoryRouter>
         )
 
-        // listing name visible
-        expect(screen.getByText("ListingDetailsMock")).toBeInTheDocument()
+        expect(await screen.findByText("Fundraiser ends in:")).toBeInTheDocument();
+        expect(screen.queryByText("Fundraiser starts in:")).not.toBeInTheDocument();
+    })
 
-        // button hidden until hover
-        expect(screen.queryByText("Check it out!")).not.toBeInTheDocument()
+    it("renders countdown to fundraiser end when ongoing", async () => {
+        render(
+            <MemoryRouter>
+                <CountdownClock 
+                    startTime={`${"2004-07-26"}T18:27:00`}
+                    endTime={`${"2004-07-27"}T18:27:00`} 
+                />
+            </MemoryRouter>
+        )
 
-        // hover over component
-        fireEvent.mouseEnter(screen.getByText("ListingDetailsMock").parentElement)
-
-        expect(screen.getByText("Check it out!")).toBeInTheDocument()
-
-        // open component
-        fireEvent.click(screen.getByText("Check it out!"))
-        expect(screen.getAllByText("ListingDetailsMock")).toHaveLength(2)
-
-        // see congrats 
-        expect(screen.getByText("Better luck next time!")).toBeInTheDocument();
+        expect(await screen.findByText("Fundraiser ended")).toBeInTheDocument();
+        expect(screen.queryByText("Fundraiser starts in:")).not.toBeInTheDocument();
+        expect(screen.queryByText("Fundraiser ends in:")).not.toBeInTheDocument();
     })
 })
