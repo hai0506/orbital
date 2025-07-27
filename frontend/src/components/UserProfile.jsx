e1f8ff7576fa88c7f3434a8b1017f756effeef27
Hai <hoanghai050605@gmail.com>
2025-07-27 12:46:57 +0700

    change check it out buttons

--- frontend/src/components/UserProfile.jsx
+++ frontend/src/components/UserProfile.jsx
@@ -29,7 +29,7 @@ const UserProfile = ({ profile }) => {
                     style={{ marginTop: "10px" }} 
                     className="inline-flex items-center gap-2 rounded-md bg-gray-700 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:not-data-focus:outline-none data-focus:outline data-focus:outline-white data-hover:bg-gray-600 data-open:bg-gray-700"
                 >
-                    Check it out!
+                    Open profile
                 </Button>
             )}
         </div>
