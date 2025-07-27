bcb72c974831a82236ca56e9c35e56f9e17951c8
ruissouw <rohit16@live.com>
2025-07-27 12:42:45 +0800

    rewrite transactions tab

--- frontend/src/pages/VendorFundraiser.jsx
+++ frontend/src/pages/VendorFundraiser.jsx
@@ -1,19 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import api from '../api'
 import { useParams } from 'react-router-dom';
-import { Button } from '@headlessui/react'
 import Layout from "@/components/Layout";
 import ListingDetails from "@/components/ListingDetails";
-import { Input } from "@/components/ui/input"
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select"
 import {
   Tabs,
   TabsList,
@@ -27,6 +16,8 @@ import Review from '@/components/Review';
 import LeftReview from '@/components/LeftReview';
 import Cart from '@/components/Cart';
 import InventoryTab from '@/components/InventoryTab';
+import TransactionsTab from '@/components/TransactionsTab';
+import Receipt from '@/components/Receipt';
 
 // comment this out to test api
 // import transactions from '@/data/Transactions';
@@ -242,111 +233,16 @@ const VendorFundraiser = () => {
                                 </>
                             </TabsContent>
                             <TabsContent value="transactions">
-                                <h5 className="text-2xl font-semibold mb-2">Transactions</h5>
-                                <Input
-                                    type="search"
-                                    placeholder="Search by Buyer Name"
-                                    onChange={e => setSearchBuyer(e.target.value)}
-                                    className="w-full md:w-1/3 mb-2"
+                                <TransactionsTab
+                                    transactions={transactions}
+                                    setSearchBuyer={setSearchBuyer}
+                                    setReceipt={setReceipt}
                                 />
-                                {transactions.length > 0 && (
-                                    <div className="max-h-[60vh] overflow-auto">
-                                        <table className="w-full text-sm">
-                                            <thead className="sticky top-0 bg-gray-100">
-                                                <tr>
-                                                    <th key='name' className="p-2 text-left">Buyer Name</th>
-                                                    <th key='phone' className="p-2 text-left">Phone Number</th>
-                                                    <th key='email' className="p-2 text-left">Email Address</th>
-                                                    <th key='payment' className="p-2 text-left">Payment Method</th>
-                                                    <th key='receipt' className="p-2 text-left">Receipt</th>
-                                                    <th key='datetime' className="p-2 text-left">Time of Transaction</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                {transactions?.map(row => (
-                                                    <tr className="border-b">
-                                                        <td className="p-2">
-                                                            {row.name}
-                                                        </td>
-                                                        <td className="p-2">
-                                                            {row.phone}
-                                                        </td>
-                                                        <td className="p-2">
-                                                            {row.email}
-                                                        </td>
-                                                        <td className="p-2">
-                                                            {row.payment}
-                                                        </td>
-                                                        <td className="p-2">
-                                                            <button onClick={() => setReceipt(row)} className="text-blue-500 hover:text-blue-700">View Receipt</button>
-                                                        </td>
-                                                        <td className="p-2">
-                                                            {new Date(row.time_created).toLocaleString('en-SG', {
-                                                                dateStyle: 'medium',
-                                                                timeStyle: 'short',
-                                                            })}
-                                                        </td>
-                                                    </tr>
-                                                ))}
-                                            </tbody>
-                                        </table>
-                                    </div>
-                                )}
-                                {transactions.length <= 0 && (
-                                    <p>No transactions yet.</p>
-                                )}
                                 {receipt?.items && (
-                                    <div 
-                                        className="fixed inset-0 z-50 flex items-center justify-center bg-black/30 backdrop-blur-sm"
-                                        onClick={() => setReceipt(null)}
-                                    >
-                                        <div 
-                                            className="max-w-2xl w-full rounded-lg bg-white p-6 shadow-lg relative"
-                                            onClick={e => e.stopPropagation()}
-                                        >
-                                            <div className="flex flex-col h-full w-full">
-                                                <h5 className="text-2xl font-semibold mb-2">Receipt</h5>
-                                                <div className="max-h-[60vh] overflow-auto">
-                                                    <table className="w-full text-sm">
-                                                        <thead className="sticky top-0 bg-gray-100">
-                                                            <tr>
-                                                                <th key='receipt-item' className="p-2 text-left">Item</th>
-                                                                <th key='receipt-price' className="p-2 text-left">Unit Price</th>
-                                                                <th key='receipt-quantity' className="p-2 text-left">Quantity</th>
-                                                                <th key='receipt-cost' className="p-2 text-left">Total Cost</th>
-                                                            </tr>
-                                                        </thead>
-                                                        <tbody>
-                                                            {receipt?.items?.map(item => (
-                                                                <tr className="border-b">
-                                                                    <td className="p-2">
-                                                                        {item.product.name}
-                                                                    </td>
-                                                                    <td className="p-2">
-                                                                        ${item.product.price.toFixed(2)}
-                                                                    </td>
-                                                                    <td className="p-2">
-                                                                        {item.quantity}
-                                                                    </td>
-                                                                    <td className="p-2">
-                                                                        ${item.total_price.toFixed(2)}
-                                                                    </td>
-                                                                </tr>
-                                                            ))}
-                                                        </tbody>
-                                                    </table>
-                                                </div>
-                                                <p className="text-2xl font-semibold mt-6 mb-2">Total Price: ${receipt.total_price.toFixed(2)}</p>
-                                            </div>
-
-                                            <button
-                                                onClick={() => setReceipt(null)}
-                                                className="absolute top-2 right-2 text-gray-500 hover:text-black"
-                                            >
-                                                <X/>
-                                            </button>
-                                        </div>
-                                    </div>
+                                    <Receipt
+                                        receipt={receipt}
+                                        setReceipt={setReceipt}
+                                    />
                                 )}
                             </TabsContent>
                             <TabsContent value="statistics">
